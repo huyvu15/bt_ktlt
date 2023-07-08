bc313ea34078fb55637b980cd940d74df1f72905
VuHuy <huyv80313@gmail.com>
2023-07-08 10:39:37 +0700

    upload

--- Turn_3/3.3.cpp
+++ Turn_3/3.3.cpp
@@ -1,49 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main() {
-    FILE *binaryFile; // con tr? t?p nh? phân
-    double number; // s? th?p phân
+int main(){	
+/*	FILE *f;
+	//1 - tao file
+	//2 - mo file
+	f = fopen("Bai3_3.bin", "w");
+	if(f == NULL){
+		printf("\n Have error");
+		exit(1);
+	}
+	// 3 - doc - ghi
+	int n;
+	printf("\n Nhap n:");
+	scanf("%d", &n);
+	
+	fprintf(f, "%d", n); // file ghi, kieu du lieu, ghi vao gi
+	
+	// 4 - Dong file
+	fclose(f);
 
-    // Nh?p các s? th?p phân t? bàn phím và ghi vào t?p nh? phân
-    printf("Nh?p các s? th?p phân (nh?p 0 d? k?t thúc):\n");
-
-    // M? t?p nh? phân d? ghi
-    binaryFile = fopen("numbers.txt", "wb");
-    if (binaryFile == NULL) {
-        printf("Không th? m? t?p nh? phân.\n");
-        return 1;
-    }
-
-    while (1) {
-        printf("? ");
-        scanf("%lf", &number);
-
-        if (number == 0) {
-            break; // K?t thúc khi nh?p s? 0
-        }
-
-        // Ghi s? th?p phân vào t?p nh? phân
-        fwrite(&number, sizeof(double), 1, binaryFile);
-    }
-
-    fclose(binaryFile); // Ðóng t?p nh? phân sau khi ghi
-
-    // Ð?c t?p nh? phân và in ra các s? dã nh?p
-    printf("\nCác s? dã nh?p t? t?p nh? phân:\n");
-
-    // M? t?p nh? phân d? d?c
-    binaryFile = fopen("numbers.txt", "rb");
-    if (binaryFile == NULL) {
-        printf("Không th? m? t?p nh? phân.\n");
-        return 1;
-    }
-
-    while (fread(&number, sizeof(double), 1, binaryFile) == 1) {
-        printf("%.2f\n", number);
-    }
-
-    fclose(binaryFile); // Ðóng t?p nh? phân sau khi d?c
-
-    return 0;
+*/	
+	return 0;
+	
+	
+	
+	
+	
+	
 }
-
